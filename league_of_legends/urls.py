2da8d272c6be87a739e5349d4a0c4f0590d4e885
Ryan <rfriel92@gmail.com>
2020-10-22 01:24:38 -0400

    Update core urls.py for frontend and backend.

--- league_of_legends/urls.py
+++ league_of_legends/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('frontend.urls', 'frontend')),
     path('', include('backend.urls', 'backend')),
     # path('accounts/', include('allauth.urls')),
 ]
