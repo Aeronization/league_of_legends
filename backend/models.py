d5151ee6081aaab1604f389c133132115619d718
Ryan <rfriel92@gmail.com>
2020-10-22 01:36:55 -0400

    Update primary key of the Champion model to the name of the champion. Its always unique.

--- backend/models.py
+++ backend/models.py
@@ -8,7 +8,7 @@ from django.db import models
 # Create your models here.
 class Champion(models.Model):
     key = models.IntegerField(null=True, blank=True)
-    name = models.CharField(max_length=255, null=True, blank=True)
+    name = models.CharField(max_length=255, default=None, primary_key=True)
     title = models.CharField(max_length=255, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
 
