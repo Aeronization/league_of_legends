2a52dd6c5e4a52f53a25734dafaa850fcead2459
Ryan <rfriel92@gmail.com>
2020-10-23 17:26:42 -0400

    Include champion model in the views for future development.

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from backend.models import Champion
 
 def index(request):
+    # Champion.process_specific_champion("Aatrox")
     return render(request, 'frontend/index.html')
