5435008e38d996233580de5f9b839cd786034492
Ryan <rfriel92@gmail.com>
2020-10-23 17:26:14 -0400

    Add constants for URL endpoints for riot games API.

--- backend/constants.py
+++ backend/constants.py
@@ -1,1 +1,6 @@
 API_ENDPOINT = 'api/champions'
+LOL_VERSION_URL = "https://ddragon.leagueoflegends.com/api/versions.json"
+CHAMPION_PRE_URL = "http://ddragon.leagueoflegends.com/cdn/"
+CHAMPION_POST_URL = "/data/en_US/champion.json"
+SPECIFIC_CHAMPION_POST_URL = "/data/en_US/champion/"
+CHAMPION_SPLASH_URL = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/"
